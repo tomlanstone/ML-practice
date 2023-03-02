5a63d000dbf02ca020c9f4588dec02296c53ee66
Tom Lanstone <thomaslanstone@hotmail.co.uk>
2023-03-02 14:22:14 +0000

    up to q21

--- NLTK textbook progress/Chapter 1/Exercises.py
+++ NLTK textbook progress/Chapter 1/Exercises.py
@@ -1,5 +1,5 @@
 import nltk 
-#from nltk.book import *
+from nltk.book import *
 import matplotlib.pyplot as plt
 
 # 4
@@ -70,4 +70,70 @@ def q_13():
     print(ans.phrase1[4][0])
 
 ## 14
+
+def all_index(sent,word):
+    print([i for i, x in enumerate(sent) if x.lower() == word])
+
+## 15
+
+def q_15():
+    a = [i for i in text5 if i.lower().startswith("b")]
+    print(sorted(set(a)))
+
+## 17 
+# finds all occurences of the word in the given text, searches for the start and end of the sentence for each one, returns them as a list of lists. Should be very easy to add an extra for loop to handle something such as a corpus of product reviews
+
+class contextualiser():
+    def __init__(self,text,word):
+        self.word = word
+        self.text = text
+        self.indexes = [i for i, x in enumerate(text) if x.lower() == self.word]
+    
+    def get_context(self):
+        sentences = [self.make_sentence(i) for i in self.indexes]
+        return sentences
+            
+    def make_sentence(self, i):
+        start = self._sentence_start(i)
+        end = self._sentence_end(i)
+        sentence = [w for w in self.text[start:end]]
+        return sentence
+    
+    def _sentence_start(self, i):
+        punctuation = [".","!","?"]
+        count = i
+        token = self.text[count]
+        while token not in punctuation:
+            token = self.text[count]
+            count -=1
+        if token in punctuation:
+            start = count + 2
+        return start
+    
+    def _sentence_end(self, i):
+        punctuation = [".","!","?"]
+        count = i
+        token = self.text[count]
+        while token not in punctuation:
+            token = self.text[count]
+            count +=1
+        if token in punctuation:
+            end = count 
+        return end
+
+## 18
+
+def q_18():
+    ans = sent1 + sent2 + sent3 + sent4 + sent5 + sent6 + sent7 + sent8
+    a = len(ans)
+    ans_b = sorted(set(ans))
+    b = len(ans_b)
+    print(b/a)
+
+## 21
+
+def q_21():
+    a = text2[-2:]
+    print(a)
+q_21()
 
